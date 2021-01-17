1069b7342748b3e1ccb5faaae387b5a5c608071a
Kgebski <kgebski68@gmail.com>
2021-01-17 22:45:54 +0100

    new controls

--- Pająk/Scripts/Spider.gd
+++ Pająk/Scripts/Spider.gd
@@ -1,26 +1,54 @@
 extends KinematicBody
 
-var velocity = Vector3(0,0,0)
-const SPEED = 10
-func _ready():
-	pass
+var direction = Vector3.BACK
+var velocity = Vector3.ZERO
+var strafe_dir = Vector3.ZERO
+
+
+var vertical_velocity = 0
+var gravity = 20
+
+var movement_speed = 0
+var walk_speed = 20
+var run_speed = 30
+var acceleration = 6
+
+func _input(event):
+		if event.is_action_released("sprint"):
+				velocity = direction
 
 func _physics_process(delta):
 	
-	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_right"):
-		velocity.x = -SPEED
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = SPEED
+	if !$roll_timer.is_stopped():
+		acceleration = 3.5
 	else:
-		velocity.x = lerp(velocity.x,0,0.1)
-	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
-		velocity.z = 0
-	elif Input.is_action_pressed("ui_up"):
-		velocity.z = SPEED
-	elif Input.is_action_pressed("ui_down"):
-		velocity.z = -SPEED
+		acceleration = 5
+	
+	var h_rot = $Camroot/h.global_transform.basis.get_euler().y
+	
+	if Input.is_action_pressed("forward") ||  Input.is_action_pressed("backward") ||  Input.is_action_pressed("left") ||  Input.is_action_pressed("right"):
+		
+		direction = Vector3(Input.get_action_strength("right") - Input.get_action_strength("left"),
+					0,
+					Input.get_action_strength("backward") - Input.get_action_strength("forward"))
+
+		strafe_dir = direction
+		
+		direction = direction.rotated(Vector3.UP, h_rot).normalized()
+		
+		if Input.is_action_pressed("sprint"):
+			movement_speed = run_speed
+		else:
+			movement_speed = walk_speed
 	else:
-		velocity.z = lerp(velocity.z,0,0.1)
-	move_and_slide(velocity)
+		movement_speed = 0
+		strafe_dir = Vector3.ZERO
+	
+	velocity = lerp(velocity, direction * movement_speed, delta * acceleration)
+
+	move_and_slide(velocity + Vector3.DOWN * vertical_velocity, Vector3.UP)
+	
+	if !is_on_floor():
+		vertical_velocity += gravity * delta
+	else:
+		vertical_velocity = 0
