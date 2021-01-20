95f3004abe7d7df8b26f8e99490bc541acb4389f
Kgebski <kgebski68@gmail.com>
2021-01-20 15:32:35 +0100

    sterowanie is good

--- Pajonk/Player/Player.gd
+++ Pajonk/Player/Player.gd
@@ -8,16 +8,18 @@ var strafe = Vector3.ZERO
 
 var speed = 0
 var walk_speed = 15
-var run_speed = 30
 
 var aim_turn = 0
 
 var acceleration = 4
 
-var vertical_velocity = 0
-#var gravity = 1
-
 var angular_acceleration = 3
+
+onready var tree = $AnimationTree
+onready var mesh = $Mesh
+onready var camroot = $Camroot/h
+
+var is_aiming = false
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -25,19 +27,19 @@ func _input(event):
 
 func _physics_process(delta):
 	if (Input.is_action_pressed("aim")):
-		$AnimationTree.set("parameters/aim_transmition/current", 0)
+		tree.set("parameters/aim_transmition/current", 0)
+		is_aiming = true
 	else:
-		$AnimationTree.set("parameters/aim_transmition/current", 1)
+		tree.set("parameters/aim_transmition/current", 1)
+		is_aiming = false
 		
-	var cam_rotation = $Camroot/h.global_transform.basis.get_euler().y
+	var cam_rotation = camroot.global_transform.basis.get_euler().y
 	
 	if (Input.is_action_pressed("left") ||  Input.is_action_pressed("right") || Input.is_action_pressed("foreward") || Input.is_action_pressed("backward")):
-		if(Input.is_action_pressed("sprint") && $AnimationTree.get("parameters/aim_transmition/current") == 1):
-			speed = run_speed
-
+		if(Input.is_action_pressed("sprint") && !is_aiming):
+			speed = walk_speed * 2
 		else:
 			speed = walk_speed
-
 			
 		direction = Vector3(Input.get_action_strength("left") - Input.get_action_strength("right"),0,Input.get_action_strength("foreward") - Input.get_action_strength("backward"))
 		
@@ -48,28 +50,29 @@ func _physics_process(delta):
 		speed = 0
 		strafe_dir = Vector3.ZERO
 		
-		if ($AnimationTree.get("parameters/aim_transmition/current") == 0):
-			direction = $Camroot/h.global_transform.basis.z
+		if (is_aiming):
+			direction = camroot.global_transform.basis.z
 	
 	velocity = lerp(velocity, direction * speed, delta * acceleration)
 		
-	move_and_slide(velocity + Vector3.DOWN * vertical_velocity, Vector3.UP)
+	move_and_slide(velocity, Vector3.UP)
 	
-	if($AnimationTree.get("parameters/aim_transmition/current") == 1):
-		$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, atan2(direction.x, direction.z), delta * angular_acceleration)
+	if(!is_aiming):
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(direction.x, direction.z), delta * angular_acceleration)
 	else:
-		$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, cam_rotation, delta * angular_acceleration)
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, cam_rotation, delta * angular_acceleration)
 		
 	strafe = lerp(strafe, strafe_dir + Vector3.RIGHT * aim_turn, delta * acceleration)
 
-	$AnimationTree.set("parameters/strafe/blend_position", Vector2(-strafe.x, strafe.z))
+	tree.set("parameters/strafe/blend_position", Vector2(-strafe.x, strafe.z))
 	
 	var iw_blend = (velocity.length() - walk_speed) / walk_speed
-	var wr_blend = (velocity.length() - walk_speed) / (run_speed -walk_speed)
+	var wr_blend = (velocity.length() - walk_speed) / (walk_speed * 2 - walk_speed)
 		
 	if velocity.length() <= walk_speed:
-		$AnimationTree.set("parameters/Blend3/blend_amount", iw_blend)
+		tree.set("parameters/Blend3/blend_amount", iw_blend)
 	else:
-		$AnimationTree.set("parameters/Blend3/blend_amount", wr_blend)
+		tree.set("parameters/Blend3/blend_amount", wr_blend)
+		
 	aim_turn = 0
 	
