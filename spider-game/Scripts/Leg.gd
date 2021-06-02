85d81a85956d1e88255ef88492030557026f9111
Jacek Jarosz <jacekjarosz55@gmail.com>
2021-06-02 19:54:42 +0200

    Added some not working stuff just usual day of work

--- spider-game/Scripts/Leg.gd
+++ spider-game/Scripts/Leg.gd
@@ -3,9 +3,10 @@ extends Spatial
 export(NodePath) var target_path_relative
 
 onready var IK = $Armature/Skeleton/SkeletonIK
-onready var RayCastFront = $RayFront
+onready var raycast = $Ray
 onready var whereisgoal = $WHERESGOAL
-
+onready var whereitfuckinghits = $WHEREITFUCKINGHITS
+onready var player = get_parent()
 onready var target_path_absolute = "../../../" + target_path_relative
 onready var target : Spatial = get_node(target_path_relative) 
 
@@ -19,9 +20,9 @@ var targetpoint : Vector3 = Vector3.ZERO
 #var leg_index = name.substr(3)
 
 func _ready():
-	if(RayCastFront.is_colliding()):
-		RayCastFront.force_raycast_update()
-		hitpoint = RayCastFront.get_collision_point()
+	if(raycast.is_colliding()):
+		raycast.force_raycast_update()
+		hitpoint = raycast.get_collision_point()
 	else:
 		hitpoint = Vector3.ZERO
 	goalpoint = hitpoint
@@ -30,11 +31,15 @@ func _ready():
 	IK.start()
 #
 func _physics_process(_delta):
-	if RayCastFront.is_colliding():
-		hitpoint = RayCastFront.get_collision_point()
+	
+	raycast.cast_to = Vector3(player.move_vector.x,-1,-player.move_vector.z) * 15
+	print(raycast.cast_to)
+	if raycast.is_colliding():
+		hitpoint = raycast.get_collision_point()
 		if goalpoint.distance_to(hitpoint) > DISTANCE:
 			goalpoint = hitpoint + Vector3.UP*0.5
 	targetpoint = targetpoint.linear_interpolate(goalpoint,SMOOTHNESS)
 	target.global_transform.origin = targetpoint
 	whereisgoal.global_transform.origin = goalpoint
+	whereitfuckinghits.global_transform.origin = hitpoint
 	#target.global_transform.origin = target.global_transform.origin.linear_interpolate(goalpoint,SMOOTHNESS)
